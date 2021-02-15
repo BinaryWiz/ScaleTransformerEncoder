e98109d9dd985d72344236ff0ecda0e65019d8ea
BinaryWiz <ultimi450@gmail.com>
2021-02-15 15:33:33 -0500

    able to either use the heads or the last linear layer to scale the dimensions of the output

--- src/scaling_layer.py
+++ src/scaling_layer.py
@@ -4,16 +4,18 @@ from multihead import MultiHeadSelfAttn
 from position_wise_feed_forward import PositionWiseFeedForward
 
 class ScalingLayer(nn.Module):
-    def __init__(self, in_features, out_features, pwff_inner_features, pwff_dropout=0.1):
+    def __init__(self, in_features, out_features, pwff_inner_features, head_scale=False, pwff_dropout=0.1):
         super(ScalingLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.pwff_inner_features = pwff_inner_features
         self.pwff_dropout = pwff_dropout
+        self.head_scale = head_scale
 
         # Multi-Head Self Attention
         self.multihead = MultiHeadSelfAttn(in_features=self.in_features,
-                                           out_features=self.out_features)
+                                           out_features=self.out_features,
+                                           head_scale=self.head_scale)
         
         # Position-Wise Feed Forward
         self.pwff = PositionWiseFeedForward(in_features=self.out_features,
@@ -22,8 +24,9 @@ class ScalingLayer(nn.Module):
                                             dropout=self.pwff_dropout)
         
         # This is used to scale the original embedding to make a residual connection if in_features != out_features
-        self.residual_scale = nn.Linear(in_features=self.in_features,
-                                        out_features=self.out_features)
+        if self.in_features != self.out_features:
+            self.residual_scale = nn.Linear(in_features=self.in_features,
+                                            out_features=self.out_features)
         
         # The Layer Normalization layers
         self.multihead_ln = nn.LayerNorm(self.out_features)
@@ -36,8 +39,10 @@ class ScalingLayer(nn.Module):
         # Forward propagate through the multihead attention layer
         out = self.multihead(embeddings)
         
-        # Scale the original embeddings down to add element-wise
-        residual = self.residual_scale(residual)
+        # Scale the original embeddings down to add element-wise (if in_features != out_features)
+        if self.in_features != self.out_features:
+            residual = self.residual_scale(residual)
+        
         out = self.multihead_ln(out + residual)
         
         # Keep a copy of out now to add after the Position-Wise Feed Forward
@@ -49,3 +54,9 @@ class ScalingLayer(nn.Module):
         # Complete the residual connection and apply Layer Normalization
         out = self.pwff_ln(out + residual)
         return out
+
+if __name__ == "__main__":
+    x = torch.randn(16, 40, 768)
+    scale = ScalingLayer(768, 512, 2048, head_scale=True)
+    print("Input size: {}".format(x.size()))
+    print("Output size: {}".format(scale(x).size()))
